9862c700b158f9e8aaf901287d425e0b2ac63e06
Brian Whicheloe <brian@Brians-MacBook-Pro-3.local>
2017-03-11 23:05:31 -0800

    Fix analytics id switching

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
+/* globals ENV */
 import Vue from 'vue'
 import router from './routes.js'
 import app from './components/app.vue'
 import VueAnalytics from 'vue-analytics'
 
-let id = process.env.NODE_ENV === 'production' ? 'UA-84589929-1' : 'UA-93531536-2'
+let id = ENV === 'production' ? 'UA-84589929-1' : 'UA-93531536-2'
 Vue.use(VueAnalytics, {id, router})
 
 new Vue({
