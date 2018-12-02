65875850d163f5304e33b8a91e1c3b597362ba78
Brian Whicheloe <brian@whicheloe.us>
2018-12-02 15:56:12 -0800

    Remove yarn and change default port to 3006

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const historyFallback = require('connect-history-api-fallback')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3006
 app.use(historyFallback())
 
 app.use(express.static(path.resolve(`${__dirname}/public`)))
